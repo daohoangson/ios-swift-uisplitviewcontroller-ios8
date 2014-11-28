8786b2a2577f959580b64b0b39164dc63f641fb3
Dao Hoang Son <daohoangson@gmail.com>
2014-11-29 02:23:10 +0700

    Initial Commit

---
+++ Master Detail ApplicationTests/Master_Detail_ApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Master_Detail_ApplicationTests.swift
+//  Master Detail ApplicationTests
+//
+//  Created by Dao Hoang Son on 11/29/14.
+//  Copyright (c) 2014 Dao Hoang Son. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Master_Detail_ApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
